0dbc89a2a8cd4a2576372905320292e22bba4f4b
J2ghz <j2.00ghz@gmail.com>
2019-02-13 16:36:50 +0100

    read files

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ShellProgressBar;
 
 namespace MailIndexer
 {
     class Program
     {
+        static IEnumerable<FileInfo> Crawl(DirectoryInfo dir){
+            foreach (var file in dir.EnumerateFiles()){
+                yield return file;
+            }
+            
+            foreach (var d in dir.EnumerateDirectories()){
+                foreach (var file in Crawl(d))
+                {
+                    yield return file;
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var root = new DirectoryInfo("/home/jghz/maildir/");
+            var files = Crawl(root).ToList();
+            using(var progresss= new ProgressBar(files.Count,"Reading files")){
+                foreach (var file in files)
+                {
+                    var text = file.OpenText().ReadToEnd();
+                    progresss.Tick();
+                }
+            }
         }
     }
 }
